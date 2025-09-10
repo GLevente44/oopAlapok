f0b91638d0650e8166dcf77bf1d1e682922c9ec2
GLevente44 <gecsel@kkszki.hu>
2025-09-10 08:30:44 +0200

    Feladat1 is ready.

--- OpAlapok/Program.cs
+++ OpAlapok/Program.cs
@@ -8,8 +8,22 @@ namespace OpAlapok
 {
     internal class Program
     {
+
+        public class Szemely
+        {
+            public string nev = "Levi";
+            public int kor = 1000;
+        }
+
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine($"A tanuló neve: {tanulo1.nev} és {tanulo1.kor} éves");
+
+
+
+
+
         }
     }
 }
