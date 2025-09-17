4942a9964467464dbf6d128ec53d6122aac7cdd2
GLevente44 <gecsel@kkszki.hu>
2025-09-17 09:40:49 +0200

    Feladat 10 is ready

--- OpAlapok/Program.cs
+++ OpAlapok/Program.cs
@@ -15,6 +15,13 @@ namespace OpAlapok
             protected string nev;
             private int kor;
 
+            public Szemely(string name, int age) 
+            {
+                nev = name;
+                kor = age;
+            }
+
+
             public string Nev { 
 
                 get { return nev; } 
@@ -75,6 +82,11 @@ namespace OpAlapok
         {
             private string neptunKod;
 
+            public Hallgato(string name, int age, string neptuncode) : base(name, age) 
+            {
+                NeptuKod = neptuncode;
+            }
+
             public string NeptuKod
             {
                 get { return neptunKod; }
@@ -89,53 +101,75 @@ namespace OpAlapok
                     }
                 }
             }
-            public void Kiir()
+            public override string ToString()
             {
-                nev = string.Empty;
-            }
+                return $"A hallgató neve: {nev} a nepunkódja: {neptunKod}";
+            }   
+            
+            
         }
 
+        public class Dolgozo : Szemely
+        {
+            public int ber;
+            public Dolgozo(string name, int age, int salary) : base(name, age) 
+            {
+                ber = salary;
+                    
+            }
+
+
+        }
 
 
         static void Main(string[] args)
         {
-            Szemely tanulo1 = new Szemely();
-            tanulo1.Nev = "Gábor";
-            tanulo1.Kor = 20;
-            Console.WriteLine(tanulo1);
+            //Szemely tanulo1 = new Szemely();
+            //tanulo1.Nev = "Gábor";
+            //tanulo1.Kor = 20;
+            //Console.WriteLine(tanulo1);
 
-            Console.WriteLine("--------------------");
+            //Console.WriteLine("--------------------");
 
             Bankszamla bankszamla1 = new Bankszamla();
             bankszamla1.Egyenleg = -5700;
             Console.WriteLine(bankszamla1.Egyenleg);
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Hallgato hallgato1 = new Hallgato();
-            hallgato1.NeptuKod = "asdasd";
-            Console.WriteLine(hallgato1.NeptuKod);
+            //Hallgato hallgato1 = new Hallgato();
+            //hallgato1.NeptuKod = "asdasd";
+            //Console.WriteLine(hallgato1.NeptuKod);
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
             List<Hallgato> hallgatok = new List<Hallgato>();
 
-            for (int i = 0; i < 2; i++)
-            {
-                Hallgato hallgato = new Hallgato();
-                Console.Write($"Kérem a(z) {i+1} hallgató nevét: ");
-                hallgato.Nev = Console.ReadLine();
-                Console.Write($"Kérem a(z) {i + 1} hallgató életkorát: ");
-                hallgato.Kor = int.Parse(Console.ReadLine());
-                Console.Write($"Kérem a(z) {i + 1} hallgató neptunkódját: ");
-                hallgato.NeptuKod = Console.ReadLine();
-                hallgatok.Add(hallgato);
-            }
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    Hallgato hallgato = new Hallgato();
+            //    Console.Write($"Kérem a(z) {i+1} hallgató nevét: ");
+            //    hallgato.Nev = Console.ReadLine();
+            //    Console.Write($"Kérem a(z) {i + 1} hallgató életkorát: ");
+            //    hallgato.Kor = int.Parse(Console.ReadLine());
+            //    Console.Write($"Kérem a(z) {i + 1} hallgató neptunkódját: ");
+            //    hallgato.NeptuKod = Console.ReadLine();
+            //    hallgatok.Add(hallgato);
+            //}
 
-            foreach (var item in hallgatok)
-            {
-                Console.WriteLine($"A hallgatók neve: {item.Nev}");
-            }
+            //foreach (var item in hallgatok)
+            //{
+            //    Console.WriteLine($"A hallgatók neve: {item.Nev}");
+            //}
+
+            Console.WriteLine("-----------------------");
+
+            Hallgato hallgato1 = new Hallgato("Peti", 22, "HGJRIS");
+            Console.WriteLine(hallgato1);
+
+            Dolgozo dolgozo1 = new Dolgozo("Gábor", 34, 6800);
+            Console.WriteLine(dolgozo1);
+
         }
     }
 }
